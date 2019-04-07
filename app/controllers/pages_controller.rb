3a86686a5d51072303d7d5f4b89756249e8ec922
Louis <tran.louis@gmail.com>
2019-04-07 13:26:11 -0700

    checkbox updates task model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:update_task]
+
   def todo
     @tasks = Task.all
+  end
+
+  def update_task
+    @task = Task.find(task_params[:task_id])
+    @task.update_attributes(completed: task_params[:completed])
+  end
+
+  private
+  def task_params
+    params.permit(:completed, :task_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/todo'
   root 'pages#todo'
+
+  match 'complete/:task_id', to: 'pages#update_task', via: [:patch]
+
   resources :tasks
   resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
